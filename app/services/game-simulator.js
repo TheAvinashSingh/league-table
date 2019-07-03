e902337250b7c66d05f5ed8ece8c7e8311d58d83
Avinash Singh <connecttoavinash@outlook.com>
2019-07-04 02:39:45 +0530

    game results done

--- app/services/game-simulator.js
+++ app/services/game-simulator.js
@@ -1,9 +1,18 @@
 import Service from '@ember/service';
 import { inject } from '@ember/service';
 import { later } from '@ember/runloop';
+import { shuffle } from 'ember-composable-helpers/helpers/shuffle';
+import { computed } from '@ember/object';
+
+let DELAY_BETWEEN_GAMES= 100;
 
 export default Service.extend({
   store: inject(),
+
+  games: computed(function() {
+    return this.store.peekAll('game');
+  }),
+
   init(){
     this._super(...arguments);
 
@@ -11,14 +20,42 @@ export default Service.extend({
 
     this.seedTeams();
 
-    later(this, this.simulateGame, 1000);
+    later(this, this.simulateGame, DELAY_BETWEEN_GAMES);
   },
 
   seedTeams(){
-    let teamNames = ['Manchester United', 'Team 2', 'Team 3', 'Team 4'];
+    let teamNames = ['Team 0', 'Team 2', 'Team 3', 'Team 4'];
 
     for(let i =0; i<teamNames.length; i++){
       this.store.createRecord('team', { id: i, name: teamNames[i] } )
     }
+  },
+
+  simulateGame() {
+    let teams = this.store.peekAll('team');
+
+    let shuffledTeams = shuffle(teams);
+
+    let homeTeam = shuffledTeams[0];
+    let awayTeam = shuffledTeams[1];
+
+    let homeGoals = this.randomScore(4);
+    let awayGoals = this.randomScore(3);
+
+    console.log({ homeGoals, awayGoals});
+
+    this.store.createRecord('game', {
+      homeTeam,
+      awayTeam,
+      homeGoals,
+      awayGoals,
+      playedDate: new Date()
+    });
+
+    later(this, this.simulateGame, DELAY_BETWEEN_GAMES);
+  },
+
+  randomScore(maximumGoals) {
+    return Math.round(Math.random() * maximumGoals )
   }
 });
